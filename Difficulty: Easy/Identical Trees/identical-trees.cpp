13e0d356c160a33190dd2e3afd1c7da8b07df92f
23A91A05H8 <23A91A05H8@aec.edu.in>
2025-09-22 11:30:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Identical Trees/identical-trees.cpp
@@ -0,0 +1,31 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    bool isIdentical(Node* root1, Node* root2) {
+        // code here
+        if(root1==NULL && root2==NULL){
+            return true;
+        }
+        if(root1==NULL || root2==NULL){
+            return false;
+        }
+        return (root1->data == root2->data) &&
+           isIdentical(root1->left, root2->left) &&
+           isIdentical(root1->right, root2->right);
+        
+    }
+};
